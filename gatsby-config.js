f0fb66407de1f0d268a479476ca9b71ab379984c
Piotr F <piotr@piotrf.pl>
2018-10-16 01:24:41 +0200

    Stash setup temp home page.

--- gatsby-config.js
+++ gatsby-config.js
@@ -4,43 +4,20 @@ module.exports = {
     "gatsby-plugin-catch-links",
     "gatsby-plugin-react-helmet",
     `gatsby-plugin-offline`,
-    `gatsby-plugin-sharp`,
     `gatsby-plugin-styled-components`,
-    `gatsby-plugin-twitter`,
     {
       resolve: `gatsby-plugin-google-fonts`,
       options: {
         fonts: [
-          `Press Start 2P`
-          // `Press Start 2P\:300,400,400i,700` // you can also specify font weights and styles
+          `Overpass:300,400,400i,700` // you can also specify font weights and styles
         ]
       }
     },
-    `gatsby-transformer-sharp`,
     {
       resolve: `gatsby-source-filesystem`,
       options: {
         path: `${__dirname}/src/pages`,
         name: "pages"
-      }
-    },
-    {
-      resolve: "gatsby-transformer-remark",
-      options: {
-        plugins: [
-          `gatsby-remark-copy-images`,
-          `gatsby-remark-autolink-headers`,
-          `gatsby-remark-copy-linked-files`,
-          `gatsby-remark-emoji`,
-          `gatsby-remark-images`,
-          {
-            resolve: "gatsby-remark-external-links",
-            options: {
-              target: "_self",
-              rel: "nofollow"
-            }
-          }
-        ]
       }
     }
   ]
--- src/pages/tldr/20180000-hello-sheepswarm/post.md
+++
@@ -1,8 +0,0 @@
----
-uid: hello-sheepswarm
-
-date: 2018-08-01T00:00:00.000Z
-title: Hello Sheepswarm
----
-
-It’s our first blog post.
